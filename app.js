e037f09642bd0de8c1b5c6f3524a64931b9df07e
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-08-06 02:00:02 -0500

    creation of api badges

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3050;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// MySql
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootpass",
+  database: "node20_mysql",
+});
+
+// Route
+app.get("/", (req, res) => {
+  res.send("Welcome to my API BADGES!");
+});
+
+// all customers
+app.get("/badges", (req, res) => {
+  const sql = "SELECT * FROM badges";
+
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send("Not result");
+    }
+  });
+});
+
+app.get("/badges/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM badges WHERE id = ${id}`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send("Not result");
+    }
+  });
+});
+
+app.post("/add", (req, res) => {
+  const sql = "INSERT INTO badges SET ?";
+
+  const badgesObj = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    jobTitle: req.body.jobTitle,
+    twitter: req.body.city,
+  };
+
+  connection.query(sql, badgesObj, (error) => {
+    if (error) throw error;
+    res.send("Badges created!");
+  });
+});
+
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, email, jobTitle, twitter } = req.body;
+  const sql = `UPDATE badges SET firstName = '${firstName}', lastName='${lastName}', email='${email}', jobTitle='${jobTitle}', twitter='${twitter}' WHERE id =${id}`;
+
+  connection.query(sql, (error) => {
+    if (error) throw error;
+    res.send("Badges updated!");
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = `DELETE FROM badges WHERE id= ${id}`;
+
+  connection.query(sql, (error) => {
+    if (error) throw error;
+    res.send("Delete badges");
+  });
+});
+
+// Check connect
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Database server running!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
